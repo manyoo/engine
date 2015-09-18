60039257b20a69c8a1cdaf8b7932fd863cc72d84
Eric Wong <ericsyw@gmail.com>
2015-09-18 23:20:25 +0800

    Change the Curves functions into string names so they won't be changed to arbitary names when using the closure compiler

--- transitions/Curves.js
+++ transitions/Curves.js
@@ -66,189 +66,189 @@
  * @property {Function} flat            - Useful for delaying the execution of
  *                                        a subsequent transition.
  */
-var Curves = {
-    linear: function(t) {
-        return t;
-    },
+var Curves = {};
 
-    easeIn: function(t) {
-        return t*t;
-    },
+Curves['linear'] = function(t) {
+    return t;
+};
 
-    easeOut: function(t) {
-        return t*(2-t);
-    },
+Curves['easeIn'] = function(t) {
+    return t*t;
+};
 
-    easeInOut: function(t) {
-        if (t <= 0.5) return 2*t*t;
-        else return -2*t*t + 4*t - 1;
-    },
+Curves['easeOut'] = function(t) {
+    return t*(2-t);
+};
 
-    easeOutBounce: function(t) {
-        return t*(3 - 2*t);
-    },
+Curves['easeInOut'] = function(t) {
+    if (t <= 0.5) return 2*t*t;
+    else return -2*t*t + 4*t - 1;
+};
 
-    spring: function(t) {
-        return (1 - t) * Math.sin(6 * Math.PI * t) + t;
-    },
+Curves['easeOutBounce'] = function(t) {
+    return t*(3 - 2*t);
+};
 
-    inQuad: function(t) {
-        return t*t;
-    },
+Curves['spring'] = function(t) {
+    return (1 - t) * Math.sin(6 * Math.PI * t) + t;
+};
 
-    outQuad: function(t) {
-        return -(t-=1)*t+1;
-    },
+Curves['inQuad'] = function(t) {
+    return t*t;
+};
 
-    inOutQuad: function(t) {
-        if ((t/=.5) < 1) return .5*t*t;
-        return -.5*((--t)*(t-2) - 1);
-    },
+Curves['outQuad'] = function(t) {
+    return -(t-=1)*t+1;
+};
 
-    inCubic: function(t) {
-        return t*t*t;
-    },
+Curves['inOutQuad'] = function(t) {
+    if ((t/=.5) < 1) return .5*t*t;
+    return -.5*((--t)*(t-2) - 1);
+};
 
-    outCubic: function(t) {
-        return ((--t)*t*t + 1);
-    },
+Curves['inCubic'] = function(t) {
+    return t*t*t;
+};
 
-    inOutCubic: function(t) {
-        if ((t/=.5) < 1) return .5*t*t*t;
-        return .5*((t-=2)*t*t + 2);
-    },
+Curves['outCubic'] = function(t) {
+    return ((--t)*t*t + 1);
+};
 
-    inQuart: function(t) {
-        return t*t*t*t;
-    },
+Curves['inOutCubic'] = function(t) {
+    if ((t/=.5) < 1) return .5*t*t*t;
+    return .5*((t-=2)*t*t + 2);
+};
 
-    outQuart: function(t) {
-        return -((--t)*t*t*t - 1);
-    },
+Curves['inQuart'] = function(t) {
+    return t*t*t*t;
+};
 
-    inOutQuart: function(t) {
-        if ((t/=.5) < 1) return .5*t*t*t*t;
-        return -.5 * ((t-=2)*t*t*t - 2);
-    },
+Curves['outQuart'] = function(t) {
+    return -((--t)*t*t*t - 1);
+};
 
-    inQuint: function(t) {
-        return t*t*t*t*t;
-    },
+Curves['inOutQuart'] = function(t) {
+    if ((t/=.5) < 1) return .5*t*t*t*t;
+    return -.5 * ((t-=2)*t*t*t - 2);
+};
 
-    outQuint: function(t) {
-        return ((--t)*t*t*t*t + 1);
-    },
+Curves['inQuint'] = function(t) {
+    return t*t*t*t*t;
+};
 
-    inOutQuint: function(t) {
-        if ((t/=.5) < 1) return .5*t*t*t*t*t;
-        return .5*((t-=2)*t*t*t*t + 2);
-    },
+Curves['outQuint'] = function(t) {
+    return ((--t)*t*t*t*t + 1);
+};
 
-    inSine: function(t) {
-        return -1.0*Math.cos(t * (Math.PI/2)) + 1.0;
-    },
+Curves['inOutQuint'] = function(t) {
+    if ((t/=.5) < 1) return .5*t*t*t*t*t;
+    return .5*((t-=2)*t*t*t*t + 2);
+};
 
-    outSine: function(t) {
-        return Math.sin(t * (Math.PI/2));
-    },
+Curves['inSine'] = function(t) {
+    return -1.0*Math.cos(t * (Math.PI/2)) + 1.0;
+};
 
-    inOutSine: function(t) {
-        return -.5*(Math.cos(Math.PI*t) - 1);
-    },
+Curves['outSine'] = function(t) {
+    return Math.sin(t * (Math.PI/2));
+};
 
-    inExpo: function(t) {
-        return (t===0) ? 0.0 : Math.pow(2, 10 * (t - 1));
-    },
+Curves['inOutSine'] = function(t) {
+    return -.5*(Math.cos(Math.PI*t) - 1);
+};
 
-    outExpo: function(t) {
-        return (t===1.0) ? 1.0 : (-Math.pow(2, -10 * t) + 1);
-    },
+Curves['inExpo'] = function(t) {
+    return (t===0) ? 0.0 : Math.pow(2, 10 * (t - 1));
+};
 
-    inOutExpo: function(t) {
-        if (t===0) return 0.0;
-        if (t===1.0) return 1.0;
-        if ((t/=.5) < 1) return .5 * Math.pow(2, 10 * (t - 1));
-        return .5 * (-Math.pow(2, -10 * --t) + 2);
-    },
+Curves['outExpo'] = function(t) {
+    return (t===1.0) ? 1.0 : (-Math.pow(2, -10 * t) + 1);
+};
 
-    inCirc: function(t) {
-        return -(Math.sqrt(1 - t*t) - 1);
-    },
+Curves['inOutExpo'] = function(t) {
+    if (t===0) return 0.0;
+    if (t===1.0) return 1.0;
+    if ((t/=.5) < 1) return .5 * Math.pow(2, 10 * (t - 1));
+    return .5 * (-Math.pow(2, -10 * --t) + 2);
+};
 
-    outCirc: function(t) {
-        return Math.sqrt(1 - (--t)*t);
-    },
+Curves['inCirc'] = function(t) {
+    return -(Math.sqrt(1 - t*t) - 1);
+};
 
-    inOutCirc: function(t) {
-        if ((t/=.5) < 1) return -.5 * (Math.sqrt(1 - t*t) - 1);
-        return .5 * (Math.sqrt(1 - (t-=2)*t) + 1);
-    },
+Curves['outCirc'] = function(t) {
+    return Math.sqrt(1 - (--t)*t);
+};
 
-    inElastic: function(t) {
-        var s=1.70158;var p=0;var a=1.0;
-        if (t===0) return 0.0;  if (t===1) return 1.0;  if (!p) p=.3;
-        s = p/(2*Math.PI) * Math.asin(1.0/a);
-        return -(a*Math.pow(2,10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/ p));
-    },
+Curves['inOutCirc'] = function(t) {
+    if ((t/=.5) < 1) return -.5 * (Math.sqrt(1 - t*t) - 1);
+    return .5 * (Math.sqrt(1 - (t-=2)*t) + 1);
+};
 
-    outElastic: function(t) {
-        var s=1.70158;var p=0;var a=1.0;
-        if (t===0) return 0.0;  if (t===1) return 1.0;  if (!p) p=.3;
-        s = p/(2*Math.PI) * Math.asin(1.0/a);
-        return a*Math.pow(2,-10*t) * Math.sin((t-s)*(2*Math.PI)/p) + 1.0;
-    },
+Curves['inElastic'] = function(t) {
+    var s=1.70158;var p=0;var a=1.0;
+    if (t===0) return 0.0;  if (t===1) return 1.0;  if (!p) p=.3;
+    s = p/(2*Math.PI) * Math.asin(1.0/a);
+    return -(a*Math.pow(2,10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/ p));
+};
 
-    inOutElastic: function(t) {
-        var s=1.70158;var p=0;var a=1.0;
-        if (t===0) return 0.0;  if ((t/=.5)===2) return 1.0;  if (!p) p=(.3*1.5);
-        s = p/(2*Math.PI) * Math.asin(1.0/a);
-        if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/p));
-        return a*Math.pow(2,-10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/p)*.5 + 1.0;
-    },
+Curves['outElastic'] = function(t) {
+    var s=1.70158;var p=0;var a=1.0;
+    if (t===0) return 0.0;  if (t===1) return 1.0;  if (!p) p=.3;
+    s = p/(2*Math.PI) * Math.asin(1.0/a);
+    return a*Math.pow(2,-10*t) * Math.sin((t-s)*(2*Math.PI)/p) + 1.0;
+};
 
-    inBack: function(t, s) {
-        if (s === undefined) s = 1.70158;
-        return t*t*((s+1)*t - s);
-    },
+Curves['inOutElastic'] = function(t) {
+    var s=1.70158;var p=0;var a=1.0;
+    if (t===0) return 0.0;  if ((t/=.5)===2) return 1.0;  if (!p) p=(.3*1.5);
+    s = p/(2*Math.PI) * Math.asin(1.0/a);
+    if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/p));
+    return a*Math.pow(2,-10*(t-=1)) * Math.sin((t-s)*(2*Math.PI)/p)*.5 + 1.0;
+};
 
-    outBack: function(t, s) {
-        if (s === undefined) s = 1.70158;
-        return ((--t)*t*((s+1)*t + s) + 1);
-    },
+Curves['inBack'] = function(t, s) {
+    if (s === undefined) s = 1.70158;
+    return t*t*((s+1)*t - s);
+};
 
-    inOutBack: function(t, s) {
-        if (s === undefined) s = 1.70158;
-        if ((t/=.5) < 1) return .5*(t*t*(((s*=(1.525))+1)*t - s));
-        return .5*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2);
-    },
+Curves['outBack'] = function(t, s) {
+    if (s === undefined) s = 1.70158;
+    return ((--t)*t*((s+1)*t + s) + 1);
+};
 
-    inBounce: function(t) {
-        return 1.0 - Curves.outBounce(1.0-t);
-    },
+Curves['inOutBack'] = function(t, s) {
+    if (s === undefined) s = 1.70158;
+    if ((t/=.5) < 1) return .5*(t*t*(((s*=(1.525))+1)*t - s));
+    return .5*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2);
+};
 
-    outBounce: function(t) {
-        if (t < (1/2.75)) {
-            return (7.5625*t*t);
-        }
-        else if (t < (2/2.75)) {
-            return (7.5625*(t-=(1.5/2.75))*t + .75);
-        }
-        else if (t < (2.5/2.75)) {
-            return (7.5625*(t-=(2.25/2.75))*t + .9375);
-        }
-        else {
-            return (7.5625*(t-=(2.625/2.75))*t + .984375);
-        }
-    },
+Curves['inBounce'] = function(t) {
+    return 1.0 - Curves.outBounce(1.0-t);
+};
 
-    inOutBounce: function(t) {
-        if (t < .5) return Curves.inBounce(t*2) * .5;
-        return Curves.outBounce(t*2-1.0) * .5 + .5;
-    },
-
-    flat: function() {
-        return 0;
+Curves['outBounce'] = function(t) {
+    if (t < (1/2.75)) {
+        return (7.5625*t*t);
     }
+    else if (t < (2/2.75)) {
+        return (7.5625*(t-=(1.5/2.75))*t + .75);
+    }
+    else if (t < (2.5/2.75)) {
+        return (7.5625*(t-=(2.25/2.75))*t + .9375);
+    }
+    else {
+        return (7.5625*(t-=(2.625/2.75))*t + .984375);
+    }
+};
+
+Curves['inOutBounce'] = function(t) {
+    if (t < .5) return Curves.inBounce(t*2) * .5;
+    return Curves.outBounce(t*2-1.0) * .5 + .5;
+};
+
+Curves['flat'] = function() {
+    return 0;
 };
 
 module.exports = Curves;
